d32a26e537fa3ea791a90b9b10c79910d5e8bc9d
Bhawna Singh <singh.bhaw@northeastern.edu>
2022-04-24 23:11:19 -0500

    Role class created

---
+++ HealthBuddy/src/HealthBuddy/models/Role/Role.java
@@ -0,0 +1,48 @@
+package HealthBuddy.Role;
+
+import javax.swing.JPanel;
+import HealthBuddy.models.UserAccount.UserAccount;
+import HealthBuddy.models.EcoSystem;
+import HealthBuddy.models.Enterprise.Enterprise;
+import HealthBuddy.models.Network.Network;
+import HealthBuddy.models.Organisation.Organisation;
+/**
+ *
+ * @author Bhawna Singh
+ */
+
+public abstract class Role {
+    public enum RoleType{
+        HealthcareAdmin("HealthcareAdmin"), 
+        Doctor("Doctor"),
+        Nurse("NURSE"),
+        Frontdesk("Frontdesk"),
+        Donor("Donor"),
+        Patient("Patient"),
+        TrustAdmin("TrustAdmin"),
+        Manager("Manager"),
+        Volunteer("Volunteer"),
+        BloodDonorCenterAdmin("BloodDonorCenterAdmin"),
+        BloodDonorCenterController("BloodDonorCenterController"),;
+        
+
+private String val;
+private RoleType(String val){
+    this.val=val;
+}
+
+@Override
+public String toString() {
+    return val; //To change body of generated methods, choose Tools | Templates.
+}
+}
+public abstract JPanel createWorkArea(JPanel userProcessContainer, 
+        UserAccount account, 
+        Organisation organisation, 
+        Enterprise enterprise,
+        Network network,
+        EcoSystem system);
+
+
+
+}
