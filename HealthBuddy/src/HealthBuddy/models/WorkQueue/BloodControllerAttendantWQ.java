6e3cb80646a5ba1abb513567fc4f3f7b2c1f294e
Bhawna Singh <singh.bhaw@northeastern.edu>
2022-04-27 19:41:55 -0400

    Added AssistantAddingTimetoWQ and BloodControllerAttendantWQ to Work Queue

---
+++ HealthBuddy/src/HealthBuddy/models/WorkQueue/BloodControllerAttendantWQ.java
@@ -0,0 +1,41 @@
+package medistopBackend.WorkQueue;
+
+/**
+ *
+ * @author Bhawna Singh
+ */
+public class BloodControllerAttendantWQ extends WorkRequest {
+    
+    private String nameOfPatient;
+    private String whichTypeOfBlood;
+    private int unitsOfBlood;
+   
+    public String getNameOfPatient() {
+        return nameOfPatient;
+    }
+
+    public void setNameOfPatient(String nameOfPatient) {
+        this.nameOfPatient = nameOfPatient;    
+    }
+    
+    public String getTypeOfBlood() {
+        return whichTypeOfBlood;
+    }
+
+    public void setTypeOfBlood(String whichTypeOfBlood) {
+        this.whichTypeOfBlood = whichTypeOfBlood;
+    }
+    
+    public int getUnitsOfBlood() {
+        return unitsOfBlood;
+    }
+
+    public void setUnitsOfBlood(int unitsOfBlood) {
+        this.unitsOfBlood = unitsOfBlood;
+    }
+    
+    @Override
+    public String toString(){
+        return nameOfPatient;
+    }
+}
