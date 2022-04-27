ab3e8702f0e0ae0876add2197f1af927ecc4fd64
Bhawna Singh <singh.bhaw@northeastern.edu>
2022-04-26 22:51:14 -0500

    Healthcare roles

---
+++ HealthBuddy/src/HealthBuddy/models/Healthcare/Organisation/HeathcareRole/HealthcareAssistant.java
@@ -0,0 +1,26 @@
+package Healthbuddy.models.Healthcare.Role;
+
+import javax.swing.JPanel;
+import Healthbuddy.models.EcoSystem;
+import Healthbuddy.models.Enterprise.Enterprise;
+import Healthbuddy.models.Hospital.Organisation.HospitalOrganisationAssistant;
+import Healthbuddy.models.Network.Network;
+import Healthbuddy.models.Organisation.Organisation;
+import Healthbuddy.models.Role.Role;
+import Healthbuddy.models.UserAccount.UserAccount;
+import Healthbuddy.ui.Assistant.AssistantWorkAreaPanel;
+
+/**
+ *
+ * @author Bhawna Singh
+ */
+public class HealthcareAssistant extends Role {
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organisation organisation, Enterprise enterprise, Network network, EcoSystem system) {
+        return new AssistantWorkAreaPanel(userProcessContainer,account,(HospitalOrganisationAssistant)organisation,enterprise,network,system); //To change body of generated methods, choose Tools | Templates.
+    }
+
+
+    
+}
