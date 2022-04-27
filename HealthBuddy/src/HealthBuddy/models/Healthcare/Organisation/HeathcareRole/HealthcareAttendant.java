22e5ee63b8b610b3130cf48fa647c6d0e0f49622
Bhawna Singh <singh.bhaw@northeastern.edu>
2022-04-27 01:39:30 -0400

    Added new roles in Healthcare

--- HealthBuddy/src/HealthBuddy/models/Healthcare/Organisation/HeathcareRole/HealthcareAttendant.java
+++ HealthBuddy/src/HealthBuddy/models/Healthcare/Organisation/HeathcareRole/HealthcareAttendant.java
@@ -3,24 +3,22 @@ package Healthbuddy.models.Healthcare.Role;
 import javax.swing.JPanel;
 import Healthbuddy.models.EcoSystem;
 import Healthbuddy.models.Enterprise.Enterprise;
-import Healthbuddy.models.Healthcare.Organisation.HealthcareOrganisationAssistant;
+import Healthbuddy.models.Healthcare.Organisation.HealthcareOrganisationAttendant;
 import Healthbuddy.models.Network.Network;
 import Healthbuddy.models.Organisation.Organisation;
 import Healthbuddy.models.Role.Role;
 import Healthbuddy.models.UserAccount.UserAccount;
-import Healthbuddy.ui.Assistant.AssistantWorkAreaPanel;
+import Healthbuddy.ui.attendant.AttendantWorkAreaPanel;
 
 /**
  *
  * @author Bhawna Singh
  */
-public class HealthcareAssistant extends Role {
+public class HealthcareAttendant extends Role{
 
     @Override
     public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organisation organisation, Enterprise enterprise, Network network, EcoSystem system) {
-        return new AssistantWorkAreaPanel(userProcessContainer,account,(HealthcareOrganisationAssistant)organisation,enterprise,network,system); //To change body of generated methods, choose Tools | Templates.
+        return new AttendantWorkAreaPanel(userProcessContainer,account,(HealthcareOrganisationAttendant) organisation,enterprise,network,system);
     }
-
-
     
 }
--- HealthBuddy/src/HealthBuddy/models/Healthcare/Organisation/HeathcareRole/HealthcareDoctor.java
+++ HealthBuddy/src/HealthBuddy/models/Healthcare/Organisation/HeathcareRole/HealthcareDoctor.java
@@ -3,24 +3,22 @@ package Healthbuddy.models.Healthcare.Role;
 import javax.swing.JPanel;
 import Healthbuddy.models.EcoSystem;
 import Healthbuddy.models.Enterprise.Enterprise;
-import Healthbuddy.models.Healthcare.Organisation.HealthcareOrganisationAssistant;
+import Healthbuddy.models.Healthcare.Organisation.HealthcareOrganisationDoctor;
 import Healthbuddy.models.Network.Network;
 import Healthbuddy.models.Organisation.Organisation;
 import Healthbuddy.models.Role.Role;
 import Healthbuddy.models.UserAccount.UserAccount;
-import Healthbuddy.ui.Assistant.AssistantWorkAreaPanel;
+import Healthbuddy.ui.doctor.DoctorWorkAreaJPanel;
 
 /**
  *
  * @author Bhawna Singh
  */
-public class HealthcareAssistant extends Role {
+public class HealthcareDoctor extends Role{
 
     @Override
     public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organisation organisation, Enterprise enterprise, Network network, EcoSystem system) {
-        return new AssistantWorkAreaPanel(userProcessContainer,account,(HealthcareOrganisationAssistant)organisation,enterprise,network,system); //To change body of generated methods, choose Tools | Templates.
+        return new DoctorWorkAreaJPanel(userProcessContainer,account,(HealthcareOrganisationDoctor) organisation,enterprise, system); //To change body of generated methods, choose Tools | Templates.
     }
-
-
     
 }
