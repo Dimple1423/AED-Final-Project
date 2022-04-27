d6240cb4bea713b4f53ab6e66583bc52dcf84882
Bhawna Singh <singh.bhaw@northeastern.edu>
2022-04-26 19:22:33 -0500

    Created Blood Donor Center

---
+++ HealthBuddy/src/HealthBuddy/models/BloodDonorCenter/BloodDonorCenterAdmin.java
@@ -0,0 +1,23 @@
+package HeathBuddy.models.BloodDonorCenter.Role;
+
+import javax.swing.JPanel;
+import HeathBuddy.models.EcoSystem;
+import HeathBuddy.models.Enterprise.Enterprise;
+import HeathBuddy.models.Network.Network;
+import HeathBuddy.models.Organisation.Organisation;
+import HeathBuddy.models.Role.Role;
+import HeathBuddy.models.UserAccount.UserAccount;
+import Healthbuddy.ui.admin.AdminWorkAreaPanel;
+
+/**
+ *
+ * @author Bhawna Singh
+ */
+public class BloodDonorCenterAdmin extends Role{
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organisation organisation, Enterprise enterprise, Network network, EcoSystem system) {
+        return new AdminWorkAreaPanel(userProcessContainer, enterprise); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
