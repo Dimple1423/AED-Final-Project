e3cc18291077745a4ef3cbac5f5870aabd2b7dca
Dimpleben Kanjibhai Patel <patel.dim@northeastern.edu>
2022-04-27 23:55:11 -0400

    Added model workqueue - fund recieve and healthcare fund

---
+++ HealthBuddy/src/HealthBuddy/models/WorkQueue/FundReceiveWQ.java
@@ -0,0 +1,70 @@
+package HealthBuddy.models.WorkQueue;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Dimple Patel
+ */
+public class FundReceiveWQ extends WorkRequest{
+    private String patientName;
+    private String funderName;
+    private String city;
+    private String hospitalName;
+    private Date dateReceived;
+    private String fundsSponsored;
+
+    public String getFundsSponsored() {
+        return fundsSponsored;
+    }
+
+    public void setFundsSponsored(String fundsSponsored) {
+        this.fundsSponsored = fundsSponsored;
+    }
+
+    public String getFunderName() {
+        return funderName;
+    }
+
+    public void setFunderName(String funderName) {
+        this.funderName = funderName;
+    }
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public void setPatientName(String patientName) {
+        this.patientName = patientName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getHospitalName() {
+        return hospitalName;
+    }
+
+    public void setHospitalName(String hospitalName) {
+        this.hospitalName = hospitalName;
+    }
+
+    public Date getDateReceived() {
+        return dateReceived;
+    }
+
+    public void setDateReceived(Date dateReceived) {
+        this.dateReceived = dateReceived;
+    }
+    
+    @Override
+    public String toString()
+    {
+        return patientName;
+    }   
+}
