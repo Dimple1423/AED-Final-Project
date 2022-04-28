588f7f15311b45e8d440b70b0df05eaf34e1a532
Nidhi Singh <singh.nidhi1@northeastern.edu>
2022-04-27 21:21:21 -0400

    Added changes for Network class

--- HealthBuddy/src/HealthBuddy/models/Healthcare/Appointment/AppointmentCatalog.java
+++ HealthBuddy/src/HealthBuddy/models/Healthcare/Appointment/AppointmentCatalog.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package HealthBuddy.Healthcare.Appointment;
+package HealthBuddy.models.Healthcare.Appointment;
 
 import java.util.ArrayList;
 
