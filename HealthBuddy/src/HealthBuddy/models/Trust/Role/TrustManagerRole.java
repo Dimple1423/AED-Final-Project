add5fb58db8a3e34029894c066e4826a1faa6cfb
Nidhi Singh <singh.nidhi1@northeastern.edu>
2022-04-29 12:55:41 -0400

    Added Healthcare org

--- HealthBuddy/src/HealthBuddy/models/Trust/Role/TrustManagerRole.java
+++ HealthBuddy/src/HealthBuddy/models/Trust/Role/TrustManagerRole.java
@@ -1,13 +1,13 @@
 package HealthBuddy.models.Trust.Role;
 
-import HealthBuddy.models.Trust.Organisation.TrustOrganisationManager;
 import HealthBuddy.models.EcoSystem;
+import HealthBuddy.models.Trust.Organisation.TrustOrgManager;
+import HealthBuddy.models.Network.Network;
 import HealthBuddy.models.Enterprise.EnterpriseTrust;
 import HealthBuddy.models.Enterprise.Enterprise;
-import HealthBuddy.models.Network.Network;
-import HealthBuddy.models.Organisation.Organisation;
 import HealthBuddy.models.Role.Role;
-import HealthBuddy.models.UserAccount.User;
+import HealthBuddy.models.Organisation.Organisation;
+import HealthBuddy.models.User.User;
 import javax.swing.JPanel;
 import HealthBuddy.models.Role.Role;
 import HealthBuddy.ui.FundManager.ManagerWorkAreaJPanel;
@@ -20,6 +20,6 @@ public class TrustManagerRole extends Role{
     @Override
     public JPanel createWorkArea(JPanel userProcessContainer, User account, Organisation organisation, Enterprise enterprise, Network network,EcoSystem system) {
         //need to write the code here:
-       return new ManagerWorkAreaJPanel(userProcessContainer,account,(TrustOrganisationManager) organisation,(FundingEnterprise) enterprise,network,system);
+       return new ManagerWorkAreaJPanel(userProcessContainer,account,(TrustOrgManager) organisation,(EnterpriseTrust) enterprise,network,system);
     }
 }
--- HealthBuddy/src/HealthBuddy/models/Trust/Role/TrustVolunteerRole.java
+++ HealthBuddy/src/HealthBuddy/models/Trust/Role/TrustVolunteerRole.java
@@ -1,13 +1,13 @@
 package HealthBuddy.models.Trust.Role;
 
-import HealthBuddy.models.Trust.Organisation.TrustOrganisationVolunteer;
-import HealthBuddy.models.EcoSystem;
 import HealthBuddy.models.Enterprise.EnterpriseTrust;
+import HealthBuddy.models.Trust.Organisation.TrustOrgVolunteer;
+import HealthBuddy.models.EcoSystem;
+import HealthBuddy.models.Organisation.Organisation;
 import HealthBuddy.models.Enterprise.Enterprise;
 import HealthBuddy.models.Network.Network;
-import HealthBuddy.models.Organisation.Organisation;
 import HealthBuddy.models.Role.Role;
-import HealthBuddy.models.UserAccount.User;
+import HealthBuddy.models.User.User;
 import javax.swing.JPanel;
 import HealthBuddy.ui.TrustVolunteer.VolunteerWorkAreaJPanel;
 
@@ -20,6 +20,6 @@ public class TrustVolunteerRole extends Role{
     public JPanel createWorkArea(JPanel userProcessContainer, User account, Organisation organisation, Enterprise enterprise,Network network, EcoSystem business)
     {
         //need to write the code here
-        return new VolunteerWorkAreaJPanel(userProcessContainer, account,(TrustOrganisationVolunteer) organisation,(FundingEnterprise) enterprise,network, business);
+        return new VolunteerWorkAreaJPanel(userProcessContainer, account,(TrustOrgVolunteer) organisation,(EnterpriseTrust) enterprise,network, business);
     }
 }
