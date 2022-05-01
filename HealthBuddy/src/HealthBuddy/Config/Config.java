826a54682e51dd68ac9449d15d695e5dd1ffc4aa
Dimpleben Kanjibhai Patel <patel.dim@northeastern.edu>
2022-05-01 18:31:02 -0400

    modified config file

--- HealthBuddy/src/HealthBuddy/Config/Config.java
+++ HealthBuddy/src/HealthBuddy/Config/Config.java
@@ -9,12 +9,12 @@ import javax.swing.JTextField;
  */
 public class Config {
 
-    public static final String accountSID = "ACf3b26aae1a938efde2f03d9190001fc7";
-    public static final String authenticationToken = "b2abc8e40ab7c59d3e5d570c6ab588e1";
-    public static String emailId = "dimple.patel1408@gmail.com";
-    public static String password = "Dimple@1423";
-    public static final String whatsappPhn = "+14155238886";
-    public static final String healthBuddyPhoneNumber = "+17124236150";
+    public static final String accountSID = "";
+    public static final String authenticationToken = "";
+    public static String emailId = "";
+    public static String password = "";
+    public static final String whatsappPhn = "";
+    public static final String healthBuddyPhoneNumber = "";
     public static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
 
